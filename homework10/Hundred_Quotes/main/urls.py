39926a3128f8017e39b03e1f6452cc2300cd0a51
OlekKruh <ksalexks@gmail.com>
2024-03-10 13:39:41 +0100

    Olek Kruh

--- homework10/Hundred_Quotes/main/urls.py
+++ homework10/Hundred_Quotes/main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_buratiny, name='hello')
+    path('', views.quotes, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('author_bio/<str:author_name>/', views.author_bio, name='author_bio'),
 ]
--- homework10/Hundred_Quotes/main/views.py
+++ homework10/Hundred_Quotes/main/views.py
@@ -1,8 +1,73 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
+import json
 
 # Create your views here.
 
+menu = [
+    {
+        'title': 'About website',
+        'url_name': 'about',
+    },
+    {
+        'title': 'Quotes',
+        'url_name': 'home'
+    },
+    {
+        'title': 'Contact',
+        'url_name': 'contact'
+    },
+    {
+        'title': 'Login',
+        'url_name': 'login'
+    },
+]
 
-def hello_buratiny(request):
-    return HttpResponse('<h1>Привет буратины!</h1>')
+with open('json/authors_quotes.json', 'r', encoding='utf-8') as file:
+    quote_db = json.load(file)
+
+with open('json/authors_bio.json', 'r', encoding='utf-8') as file:
+    bio_db = json.load(file)
+
+
+def find_in_list_of_dicts(bio_db, full_name, author_name):
+    for item in bio_db:
+        if item.get(full_name) == author_name:
+            return item
+    return None
+
+
+def quotes(request):
+    data = {
+        'title': 'Home page',
+        'menu': menu,
+        'posts': quote_db,
+        'bio': bio_db,
+    }
+    return render(request, 'main/quotes.html', context=data)
+
+
+def author_bio(request, author_name):
+    author_bio = find_in_list_of_dicts(bio_db, 'Fullname', author_name)
+
+    if not author_bio:
+        return render(request, 'main/author_not_found.html', {'author_name': author_name})
+
+    data = {
+        'menu': menu,
+        'author_bio': author_bio,
+    }
+    return render(request, 'main/authors_bio.html', context=data)
+
+
+def about(request):
+    return render(request, 'main/about.html', {'title': 'About website'})
+
+
+def contact(request):
+    return HttpResponse("Feedback")
+
+
+def login(request):
+    return HttpResponse("Authorization")
